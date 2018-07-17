6e9570a4afe38bc248e1ca72c425e19be32b478a
drivehome <arwentang821@gmail.com>
2018-07-17 23:04:17 +0800

    Push

--- Lesson 05 - Exception Basics/ex3.py
+++ Lesson 05 - Exception Basics/ex3.py
@@ -1,13 +1,24 @@
-#test3
 class RelationException(Exception):
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
+    def __init__(self, err_msg):
+        self.msg = err_msg
     def __str__(self):
-        return self.p1.p2
-        
-a = RelationException("Mommy", "Daddy")
+        return ("Are you sure that {} and {} are in love with each other?".format(p1, p2))
+
+relation = {'Jason':'Mary', 'Mary':'Jason', 'Jennifer':'Ken', 'Ken':'Jennifer', 'Tina':'Kim', 'Kim':'Tina'}
+def check(pa, pb):
+    if (pa or pa)not in relation:
+        raise Exception
+    elif relation[pa] != pb:
+        raise RelationException("Are you sure that {} and {} are in love with each other?".format(p1, p2))        
+    
 try:
-    raise a
+    p1 = input("Please enter the first person: ")
+    p2 = input("Please enter the second person: ")
+    check(p1, p2)
+    print("{} and {} are in love with each other!".format(p1, p2))
+    
 except RelationException as e:
-    print("RelationException", "Are you sure that" + a.p1 + "and" + a.p2 + "are in love with each other?")
+    print(e)
+except Exception as e:
+        print(("Are you sure that {} and {} are People?".format(p1, p2)))
+
