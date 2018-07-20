8cf957bcdefc0155d4b0494e0e491bcf6ace3c29
drivehome <arwentang821@gmail.com>
2018-07-20 22:13:19 +0800

    renew

--- Lesson07-Requests Module/ex2.py
+++ Lesson07-Requests Module/ex2.py
@@ -1,16 +1,16 @@
-ac = input ("enter account")
-pa = input("enter password")
- 
-import re 
-pattern1 = r'(?P<account>[A-Z][a-z][a-z])'
-pattern2 = r'(?P<password>[a-z][a-z][a-z][0][0-9]{5})'
- 
-match1 = re.search(pattern1,ac)
-match2 = re.search(pattern2,pa)
- 
-# print(type(match2))
-if (match1.groupdict()  is not None):
-    print(match1.groupdict())
-if (match2.groupdict()  is not None):
-    print(match2.groupdict())
- 
+import requests
+import json
+
+response = requests.get('https://www.metaweather.com/api/location/2306179/2018/7/18')
+
+highest_predictability = 0
+predictability_weather_state = "None"
+
+with open('Taipei_weather.json', 'r', encoding='utf-8-sig') as f:
+    Taipei_weather = json.load(f)
+    for row in Taipei_weather:
+        if highest_predictability < row["predictability"]:
+            highest_predictability = row["predictability"]
+            predictability_weather_state = row["weather_state_name"]
+
+print("Taipei's weather is {}".format(predictability_weather_state))
